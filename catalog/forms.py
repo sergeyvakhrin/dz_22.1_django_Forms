c02ad35c7e34efbf143f43cd1f20baf92e4fd426
sergeyvakhrin <parlamentsv@mail.ru>
2024-09-20 20:36:23 +0300

    add formset for product

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,26 +2,24 @@ from django.forms import ModelForm, forms
 
 from catalog.models import Product, Version
 
+words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 class ProductForm(ModelForm):
-
-    words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
-
 
     class Meta:
         model = Product
         fields = "__all__"
 
     def clean_product_name(self):
-        cleaned_data = self.cleaned_data['product_name']
-        for word in self.words:
+        cleaned_data = self.cleaned_data['product_name'] #TODO: реализовать метод, что бы избежать дублирования кода
+        for word in words:
             if word in cleaned_data:
                 raise forms.ValidationError('Такое название использовать нельзя')
         return cleaned_data
 
     def clean_product_description(self):
-        cleaned_data = self.cleaned_data['product_description']
-        for word in self.words:
+        cleaned_data = self.cleaned_data['product_description'] #TODO: реализовать метод, что бы избежать дублирования кода
+        for word in words:
             if word in cleaned_data:
                 raise forms.ValidationError('Такое описание использовать нельзя')
         return cleaned_data
@@ -32,3 +30,19 @@ class VersionForm(ModelForm):
     class Meta:
         model = Version
         fields = "__all__"
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name'] #TODO: реализовать метод, что бы избежать дублирования кода
+        for word in words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Такое наименование использовать нельзя')
+        return cleaned_data
+
+
+    def clean_number_version(self):
+        cleaned_data = self.cleaned_data['number_version'] #TODO: реализовать метод, что бы избежать дублирования кода
+        for word in words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Такое наименование использовать нельзя')
+        return cleaned_data
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
@@ -19,12 +20,35 @@ class ProductUpdateView(UpdateView):
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        if self.request.method == "POST":
+            context_data['formset'] = ProductFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = ProductFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_list')
 
 class ProductDetailView(DetailView):
     model = Product
+
+################# Contact ##################
 
 class ContactListView(ListView):
     model = Contact
@@ -44,8 +68,16 @@ class ContactListView(ListView):
 class ContactDetailView(ListView):
     model = Contact
 
+######################## Version #################
+
 class VersionListView(ListView):
     model = Version
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        return context_data
+
 
 class VersionCreateView(CreateView):
     model = Version
