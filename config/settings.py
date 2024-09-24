e598fc8ff2e55b1a7916021e2959680447ec347f
sergeyvakhrin <parlamentsv@mail.ru>
2024-09-24 20:32:25 +0300

    chenge db project6

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'project5',
+        'NAME': 'project6',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
