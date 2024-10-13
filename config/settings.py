96c39588d4299a6cefca43929f3f02bccbede3a4
sergeyvakhrin <parlamentsv@mail.ru>
2024-10-13 15:50:45 +0300

    settings

--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('NAME_DB'),
-        'USER': os.getenv('USER_DB'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
         'PORT': os.getenv('PORT')
