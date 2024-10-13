6c9d88058d61081f77feb1102c6f0c41db005c5e
sergeyvakhrin <parlamentsv@mail.ru>
2024-10-13 14:13:00 +0300

    add cache products

--- config/settings.py
+++ config/settings.py
@@ -159,3 +159,14 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/users/'
+
+CACHE_ENABLED = True
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",                       # redis://username:password@127.0.0.1:6379
+        "TIMEOUT": 300,
+    }
+}
+
